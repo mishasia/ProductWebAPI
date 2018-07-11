3c37675a2b62c6cd54f7094a98c5a7de1b4bb3ba
Master <Master@WIN-P524L2DV6PA>
2018-07-11 20:09:53 +0300

    Добавьте файлы проекта.

---
+++ ProductWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using ProductWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductWebAPI.Controllers
+{
+    
+    public class ValuesController : ApiController
+    {
+        ProductContext db = new ProductContext();
+        // GET api/values
+        public IEnumerable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+        // GET api/values/5
+        public Product GetProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            return product;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void CreateProduct([FromBody]Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public void EditProduct(int id, [FromBody]Product product)
+        {
+            if (id == product.Id)
+            {
+                db.Entry(product).State = EntityState.Modified;
+
+                db.SaveChanges();
+            }
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProductWebAPI/Models/ProductDbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Bogus;
+using Bogus.DataSets;
+
+namespace ProductWebAPI.Models
+{
+    public class ProductDbInitializer : DropCreateDatabaseAlways<ProductContext>
+    {
+        
+        protected override void Seed(ProductContext db)
+        {
+            var faker = new Faker();
+            for (int i = 0; i < 20; i++)
+            {
+                db.Products.Add(new Product { Name = faker.Commerce.ProductName(), Description = faker.Commerce.ProductAdjective(), Price = faker.Commerce.Price(), PictureUrl = faker.Internet.Url() });
+            }
+           /* db.Products.Add(new Product { Name = faker.Commerce.ProductName(), Description = faker.Commerce.ProductAdjective(), Price = faker.Commerce.Price(), PictureUrl = faker.Internet.Url() });
+            db.Products.Add(new Product { Name = faker.Commerce.ProductName(), Description = faker.Commerce.ProductAdjective(), Price = faker.Commerce.Price(), PictureUrl = faker.Internet.Url() });
+            db.Products.Add(new Product { Name = faker.Commerce.ProductName(), Description = faker.Commerce.ProductAdjective(), Price = faker.Commerce.Price(), PictureUrl = faker.Internet.Url() });
+            */
+            base.Seed(db);
+        }
+    }
+}
